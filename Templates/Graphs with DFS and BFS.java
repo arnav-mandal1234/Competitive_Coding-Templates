e54d184fd581bfbff002b06bb6b3176f8f345c2f
Arnav Kumar Mandal <arnav.mandal1234@gmail.com>
2018-05-22 19:15:45 +0530

    Create Graphs with DFS and BFS.java

---
+++ Templates/Graphs with DFS and BFS.java
@@ -0,0 +1,252 @@
+	                       //ARNAV KUMAR MANDAL//
+                                     //XYPHER//
+ 
+ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.io.*;
+ 
+class Solution{
+	
+	static LinkedList<Integer> l[];
+	static boolean b[];
+	static int v;
+	static long dp[];
+	
+	Solution(int n){
+		this.v = n;
+		b = new boolean[n];
+		dp = new long[n];
+		l = new LinkedList[n];
+		for(int i=0;i<n;i++)
+			l[i] = new LinkedList<Integer>();
+	}
+	
+	//	Bi-Directional
+	public static void add(int x, int y){
+		l[x].add(y);
+		l[y].add(x);
+	}
+	
+	public static void dfs(int x){
+		b[x] = true;
+		for(int k : l[x]){
+			if(!b[k]){
+				dfs(k);
+			}
+		}
+	}
+	
+	public static void bfs(int s){
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		b[s]=true;
+		queue.add(s);
+
+		while (queue.size() != 0){
+			s = queue.poll();
+			Iterator<Integer> i = l[s].listIterator();
+			while (i.hasNext())
+			{
+				int n = i.next();
+				if (!b[n]){
+					b[n] = true;
+					queue.add(n);
+				}
+			}
+		}
+	}
+	
+//--------------------------MAIN---------------------------------------------------------------------//
+	public static void main(String[] args) throws IOException {
+		Reader a= new Reader(System.in);
+		OutputWriter out = new OutputWriter(System.out);
+		//Scanner a = new Scanner(System.in);
+			
+				 
+				    	
+	}
+//--------------------------FAST IO------------------------------------------------------------------//
+	private static class Reader {
+		private InputStream stream;
+		private byte[] buf = new byte[4*1024];
+		private int curChar;
+		private int numChars;
+		private SpaceCharFilter filter;
+		
+		public Reader(InputStream stream) {
+			this.stream = stream;
+		}
+		
+		public int read() {
+			if (numChars == -1) {
+				throw new InputMismatchException();
+			}
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0) {
+					return -1;
+				}
+			}
+			return buf[curChar++];
+		}
+		
+		public int nextInt() {
+			int c = read();
+			while (isSpaceChar(c)) {
+				c = read();
+			}
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9') {
+					throw new InputMismatchException();
+				}
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+		
+		public String nextString() {
+			int c = read();
+			while (isSpaceChar(c)) {
+				c = read();
+			}
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isSpaceChar(c));
+			return res.toString();
+		}
+		
+		public double nextDouble() {
+			int c = read();
+			while (isSpaceChar(c)) {
+				c = read();
+			}
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			double res = 0;
+			while (!isSpaceChar(c) && c != '.') {
+				if (c == 'e' || c == 'E') {
+					return res * Math.pow(10, nextInt());
+				}
+				if (c < '0' || c > '9') {
+					throw new InputMismatchException();
+				}
+				res *= 10;
+				res += c - '0';
+				c = read();
+			}
+			if (c == '.') {
+				c = read();
+				double m = 1;
+				while (!isSpaceChar(c)) {
+					if (c == 'e' || c == 'E') {
+						return res * Math.pow(10, nextInt());
+					}
+					if (c < '0' || c > '9') {
+						throw new InputMismatchException();
+					}
+					m /= 10;
+					res += (c - '0') * m;
+					c = read();
+				}
+			}
+			return res * sgn;
+		}
+		
+		public long nextLong() {
+			int c = read();
+			while (isSpaceChar(c)) {
+				c = read();
+			}
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			do {
+				if (c < '0' || c > '9') {
+					throw new InputMismatchException();
+				}
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+		
+		public boolean isSpaceChar(int c) {
+			if (filter != null) {
+				return filter.isSpaceChar(c);
+			}
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+		
+		public String next() {
+			return nextString();
+		}
+		
+		public interface SpaceCharFilter {
+			public boolean isSpaceChar(int ch);
+		}
+	}
+	private static class OutputWriter {
+		private final PrintWriter writer;
+		
+		public OutputWriter(OutputStream outputStream) {
+			writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+		}
+		
+		public OutputWriter(Writer writer) {
+			this.writer = new PrintWriter(writer);
+		}
+		
+		
+		
+		public void println(Object... objects) {
+			print(objects);
+			writer.println();
+			writer.flush();
+		}
+		
+		public void print(Object... objects) {
+			for (int i = 0; i < objects.length; i++) {
+				if (i != 0) {
+					writer.print(' ');
+				}
+				writer.print(objects[i]);
+			}
+			writer.flush();
+		}
+		
+		public void close() {
+			writer.close();
+		}
+		
+		public void flush() {
+			writer.flush();
+		}
+	}
+}  
