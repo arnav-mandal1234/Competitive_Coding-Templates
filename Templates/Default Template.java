97f03201b7a57f31efc0bac8deb18b2d03c297b6
Arnav Mandal <arnav.mandal1234@gmail.com>
2020-05-26 13:27:28 +0530

    New template

--- Templates/Default Template.java
+++ Templates/Default Template.java
@@ -11,18 +11,42 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.io.*;
  
-class Solution{
-	
+public class Solution{
 //--------------------------MAIN---------------------------------------------------------------------//
 	public static void main(String[] args) throws IOException {
-		Reader a= new Reader(System.in);
-		OutputWriter out = new OutputWriter(System.out);
-		//Scanner a = new Scanner(System.in);
-			
-				    
-				
-				
+
 	}
+	
+	
+//--------------------------FAST IO objects/methods--------------------------------------------------//
+	
+	static Reader in= new Reader(System.in);
+	static OutputWriter out = new OutputWriter(System.out);
+	private static int ni(){int n = in.nextInt();return n;}
+	private static long nl(){long n = in.nextLong();return n;}
+	private double nd(){return Double.parseDouble(ns()); }
+	private static String ns(){String s = in.nextString();return s;}
+	private static int[] nai(int n){int[] a = new int[n];for(int i = 0;i < n;i++)a[i] = ni();return a;}
+	private static long[] nal(int n){long[] a = new long[n];for(int i = 0;i < n;i++)a[i] = nl();return a;}
+	private static StringBuilder sb(String s){StringBuilder S = new StringBuilder(s);return S;}
+	private static HashSet<Integer> hsi(){HashSet<Integer> s = new HashSet<Integer>();return s;}
+	private static HashSet<Long> hsl(){HashSet<Long> s = new HashSet<Long>();return s;}
+	private static HashMap<Integer,Integer> hmii(){HashMap<Integer,Integer> h = new HashMap<Integer,Integer>();return h;}
+	private static HashMap<Long,Long> hmll(){HashMap<Long,Long> h = new HashMap<Long,Long>();return h;}
+	private static HashMap<String,Integer> hmsi(){HashMap<String,Integer> h = new HashMap<String,Integer>();return h;}
+	private static int[][] nmi(int n, int m){int A[][] = new int[n][m];for(int i=0;i<n;i++){for(int j=0;j<m;j++) {A[i][j] = ni();}}return A;}
+	private static char[][] nmc(int n, int m){char A[][] = new char[n][m];for(int i=0;i<n;i++){StringBuilder s=sb(ns());for(int j=0;j<m;j++){A[i][j]=s.charAt(j);}}return A;}
+	private static void op(Object o) {out.print(o);}
+	private static void opl(Object o) {out.println(o);}
+	private static void opai(int[] o){for(int a:o)op(a+" ");opl("");}
+	private static void opad(double[] o){for(double a:o)op(a+" ");opl("");}
+	private static void opal(long[] o){for(long a:o)op(a+" ");opl("");}
+	private static void opac(char[] o){for(char a:o)op(a);opl("");}
+	private static void opas(String[] o){for(String a:o)op(a+" ");opl("");}
+	private static void opmi(int[][] o) {for(int i=0;i<o.length;i++)opai(o[i]);}
+	private static void opml(long[][] o) {for(int i=0;i<o.length;i++)opal(o[i]);}
+	private static void opmc(char[][] o) {for(int i=0;i<o.length;i++)opac(o[i]);}
+
 //--------------------------FAST IO------------------------------------------------------------------//
 	private static class Reader {
 		private InputStream stream;
