997fe6bcd6369295c1b91043df36cd2aaa54eb97
Arnav Mandal <arnav.mandal1234@gmail.com>
2020-05-27 02:26:32 +0530

    Radix sort

--- Functions.java
+++ Functions.java
@@ -33,4 +33,24 @@ public static void sieve(int n)
         }
     }
     
-    
+//-------------------------------------------Radix Sort-------------------------------------------------//
+	public static int[] radixSort(int[] f){ return radixSort(f, f.length); }
+	public static int[] radixSort(int[] f, int n)
+	{
+		int[] to = new int[n];
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(f[i]&0xffff)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[f[i]&0xffff]++] = f[i];
+			int[] d = f; f = to;to = d;
+		}
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(f[i]>>>16)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[f[i]>>>16]++] = f[i];
+			int[] d = f; f = to;to = d;
+		}
+		return f;
+	}
