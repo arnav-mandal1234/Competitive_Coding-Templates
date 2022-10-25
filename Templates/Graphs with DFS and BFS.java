bd0778054a7dc4bab6c84a68682df94c25f4a1c2
akmju98 <72348948+akmju98@users.noreply.github.com>
2022-10-25 12:07:57 +0530

    Update Graphs with DFS and BFS.java

--- Templates/Graphs with DFS and BFS.java
+++ Templates/Graphs with DFS and BFS.java
@@ -133,6 +133,28 @@ class Solution{
 			return res.toString();
 		}
 		
+				public int nextInt() {
+			int c = read();
+			while (isSpaceChar(c)) {
+				c = read();
+			}
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9') {
+					throw new InputMismatchException();
+				}
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+		
 		public double nextDouble() {
 			int c = read();
 			while (isSpaceChar(c)) {
